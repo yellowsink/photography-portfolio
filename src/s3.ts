7d4f7299f0055c7cf6e4d7049108d300abd2cade
Hazel Atkinson <yellowsink@riseup.net>
2026-04-26 01:16:47 +0100

    add the god damn authentication

--- src/s3.ts
+++ src/s3.ts
@@ -1,38 +1,52 @@
-import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
-import {DeleteObjectCommand, GetObjectCommand} from "npm:@aws-sdk/client-s3@3.1037.0";
+import {
+  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 
-const ENDPOINT = Deno.env.get("PORTFOLIO_S3_ENDPOINT") ?? "s3.us-west-004.backblazeb2.com";
+const ENDPOINT = Deno.env.get("PORTFOLIO_S3_ENDPOINT") ??
+  "s3.us-west-004.backblazeb2.com";
 const BUCKET = Deno.env.get("PORTFOLIO_S3_BUCKET") ?? "ys-photo-portfolio";
 // https://ys-photo-portfolio.s3.us-west-004.backblazeb2.com
 
-const s3Client = new S3Client({ endpoint: ENDPOINT });
+const s3Client = new S3Client({
+  endpoint: ENDPOINT,
+  credentials: {
+    accessKeyId: Deno.env.get("PORTFOLIO_S3_KEY") ?? "",
+    secretAccessKey: Deno.env.get("PORTFOLIO_S3_SECRET") ?? "",
+  },
+});
 
-export async function uploadS3(filename: string, content: ReadableStream<Uint8Array<ArrayBuffer>>) {
-	const cmd = new PutObjectCommand({
-		Body: content,
-		Key: filename,
-		Bucket: BUCKET
-	})
+export async function uploadS3(
+  filename: string,
+  content: ReadableStream<Uint8Array<ArrayBuffer>>,
+) {
+  const cmd = new PutObjectCommand({
+    Body: content,
+    Key: filename,
+    Bucket: BUCKET,
+  });
 
-	return await s3Client.send(cmd);
+  return await s3Client.send(cmd);
 }
 
 export async function deleteS3(filename: string) {
-	const cmd = new DeleteObjectCommand({
-		Key: filename,
-		Bucket: BUCKET
-	});
+  const cmd = new DeleteObjectCommand({
+    Key: filename,
+    Bucket: BUCKET,
+  });
 
-	return await s3Client.send(cmd);
+  return await s3Client.send(cmd);
 }
 
 export async function getS3(filename: string) {
-	const cmd = new GetObjectCommand({
-		Bucket: BUCKET,
-		Key: filename
-	});
+  const cmd = new GetObjectCommand({
+    Bucket: BUCKET,
+    Key: filename,
+  });
 
-	return await s3Client.send(cmd);
+  return await s3Client.send(cmd);
 }
 
 export const S3_BASE_URL = `https://${BUCKET}.${ENDPOINT}/`;
